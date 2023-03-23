6b5852d8d4ad03fbb378602f5db18316c1439784
андрей <p.aleksey2010@yandex.ru>
2023-03-23 20:37:02 +0300

    .\Innerapp.java

---
+++ Innerapp.java
@@ -0,0 +1,30 @@
+public class Innerapp {
+    IInterface v;
+
+    /***
+     * Можно проверить входной параметр @param v на принадлежность к типу входных данных. При несовпадении запустить
+     * создание дефолтного объекта по умолчанию
+     */
+//            public Innerapp(IInterface v) {
+//
+//            if (v instanceof IInterface) {
+//                this.v = v;
+//            }
+//            else {
+//                this.v = new NullInterface();
+//            }
+//        }
+
+
+    /***
+     * Совсем обойтись без конструкции if/else
+     * @param v
+     */
+    public Innerapp(IInterface v){
+        this.v =  Objects.requireNonNullElseGet(v, NullInterface::new);
+    }
+
+    void write() {
+        v.print();
+    }
+}
